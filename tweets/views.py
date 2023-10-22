6e7c0932fb2c0ef8196a26f3c144ad6ac50212c9
mohammadali <mohammadali.kalbasi6911@gmail.com>
2023-10-23 00:44:05 +0330

    22-Tweet Create Form

--- tweets/views.py
+++ tweets/views.py
@@ -2,12 +2,23 @@ import random
 from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
+
+from .forms import TweetForm
 from .models import Tweet
 
 # Create your views here.
 def home_view(request, *args,  **kwargs):
     # return HttpResponse("<h1>Hello World</h1>")
     return render(request, "pages/home.html", context={}, status=200)
+
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        # do other form related logic
+        obj.save()
+        form = TweetForm()
+    return render(request, "components/form.html", context={"form": form})
 
 
 
