40e43124aca9dc714ddc480498c22648ace52aad
mohammadali <mohammadali.kalbasi6911@gmail.com>
2023-11-04 14:33:06 +0330

    38- Django Forms to Django Framework Serializer

--- tweets/views.py
+++ tweets/views.py
@@ -6,6 +6,7 @@ from django.utils.http import is_safe_url
 
 from .forms import TweetForm
 from .models import Tweet
+from.serializers import TweetSerializer
 
 
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
@@ -15,7 +16,15 @@ def home_view(request, *args,  **kwargs):
     # return HttpResponse("<h1>Hello World</h1>")
     return render(request, "pages/home.html", context={}, status=200)
 
+
 def tweet_create_view(request, *args, **kwargs):
+    serializer = TweetSerializer(data = request.POST or None)
+    if serializer.is_valid():
+        obj = serializer.save(user=request.user) 
+        return JsonResponse(serializer.data, status=201)
+    return JsonResponse({}, status=400)
+
+def tweet_create_view_pure_django(request, *args, **kwargs):
     '''
     REST API Create View -> DRF
     
