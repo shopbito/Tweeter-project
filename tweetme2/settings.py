9622cb8e9b0f6b4a6adfc5b7ae9f8d542547cc6c
mohammadali <mohammadali.kalbasi6911@gmail.com>
2023-11-04 19:47:52 +0330

    41- Delete Tweet API View

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -127,12 +127,21 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+DEFAULT_RENDERER_CLASSES =[
+       'rest_framework.renderers.JSONRenderer',
+    ]
+
+if DEBUG:
+    DEFAULT_RENDERER_CLASSES += [
+        'rest_framework.renderers.BrowsableAPIRenderer'
+    ]
 REST_FARMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES":[
+    
+    
+    'DEFAULT_AUTHENTICATION_CLASSES':[
         'rest_framework.authentication.SessionAuthentication'
     ],
-    "DEFAULT_RENDERER_CLASSES":[
-       'rest_framework.renderers.JSONRenderer',
-    #  'rest_framework.renderers.BrowsableAPIRenderer',   
-    ]
+    'DEFAULT_AUTHENTICATION_CLASSES': DEFAULT_RENDERER_CLASSES
+    
 }
