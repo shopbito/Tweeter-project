e538110cfc1eca1b77b097f80c53047ee886dd70
mohammadali <mohammadali.kalbasi6911@gmail.com>
2023-11-21 16:26:01 +0330

    67- Create Tweet in React

--- tweetme2-web/src/lookup/components.js
+++ tweetme2-web/src/lookup/components.js
@@ -1,3 +1,19 @@
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie != '') {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+          var cookie = jQuery.trim(cookies[i]);
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) == (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
+      }
+  }
+  return cookieValue;
+}
+
 function lookup(method, endpoint, callback, data, ){
   let jsonData;
   if(data){
@@ -6,9 +22,18 @@ function lookup(method, endpoint, callback, data, ){
   const xhr = new XMLHttpRequest()
   const url = `http://localhost:8000/api${endpoint}`
   xhr.responseType = "json"
+  const csrftoken = getCookie('csrftoken');
   xhr.open(method, url)
+  xhr.setRequestHeader("Content-Type", "application/json")
+  
+  if(csrftoken){
+    xhr.setRequestHeader("HTTP_X_REQUESTED_WITH", "XMLHttpRequest")
+    xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest")
+    xhr.setRequestHeader("X-CSRFToken", csrftoken)
+  }
+  
   xhr.onload = function(){
-      callback(xhr.response, xhr.status)
+    callback(xhr.response, xhr.status)
   }
   xhr.onerror = function(e){
     console.log(e)
@@ -16,6 +41,11 @@ function lookup(method, endpoint, callback, data, ){
   }
   xhr.send(jsonData)
 }
+
+export function createTweet(newTweets, callback){
+  lookup("POST", "/tweets/create/", callback, {content: newTweets})
+}
+
 export function loadTweets(callback){
 
     lookup("GET", "/tweets/", callback)
