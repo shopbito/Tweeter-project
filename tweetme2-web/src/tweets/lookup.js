b2b0003c4f2f9dc27784b7a3196853cf6836de6c
mohammadali <mohammadali.kalbasi6911@gmail.com>
2023-11-22 10:06:30 +0330

    71 - Tweet Action Btn

--- tweetme2-web/src/tweets/lookup.js
+++ tweetme2-web/src/tweets/lookup.js
@@ -3,6 +3,12 @@ import { backendLookup } from '../lookup'
 export function apiTweetCreate(newTweets, callback){
     backendLookup("POST", "/tweets/create/", callback, {content: newTweets})
 }
+
+export function apiTweetAction(tweetId, action, callback){
+    const data = {id: tweetId, action: action}
+    backendLookup("POST", "/tweets/action/", callback, data )
+}
+  
   
 export function apiTweetList(callback){
   
