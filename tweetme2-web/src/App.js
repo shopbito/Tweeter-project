8a679bc995c6b38942fd050a98b14785358bd68d
mohammadali <mohammadali.kalbasi6911@gmail.com>
2023-12-03 00:56:55 +0330

    61-r-Handling a Form in React

--- tweetme2-web/src/App.js
+++ tweetme2-web/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import {TweetsList} from './tweets'
+import {TweetsComponent} from './tweets'
 
 
 
@@ -16,7 +16,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <div>
-          <TweetsList/>
+          <TweetsComponent/>
         </div>
         <a
           className="App-link"
