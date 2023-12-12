332616e5c83a1eb97a35b51638907955db99b427
mohammadali <mohammadali.kalbasi6911@gmail.com>
2023-12-12 16:49:09 +0330

    71-r-Tweet Action Btn

--- tweetme2-web/src/tweets/components.js
+++ tweetme2-web/src/tweets/components.js
@@ -1,6 +1,8 @@
 import React, {useEffect, useState}  from 'react';
 
-import {apiTweetCreate, apiTweetList } from './lookup';
+import {apiTweetAction,
+        apiTweetCreate, 
+        apiTweetList } from './lookup';
 
 export function TweetsComponent(props){
     const textAreaRef = React.createRef()
@@ -70,24 +72,23 @@ export function TweetsList(props){
 export function ActionBtn(props){
     const{tweet, action} = props
     const[likes, setLikes ]= useState(tweet.likes ? tweet.likes : 0)
-    const[userLike, setUserLike] = useState(tweet.userLike === true ? true :false)
+    // const[userLike, setUserLike] = useState(tweet.userLike === true ? true :false)
     const className = props.className ? props.className:'btn btn-primary btn-sm'
     const actionDisplay = action.display ? action.display : 'Action'
     
+    const handleActionBackendEvent =(response, status) =>{
+      console.log(response, status)
+      if (status === 200){
+        setLikes(response.likes)
+        //setUserLike(true)
+      }
+  
+    }
+
     const handleClick = (event) => {
         event.preventDefault()
-        if (action.type === 'like'){
-            if(userLike === true){
-                // perhaps i Unlike it?
-                setLikes(likes -1)
-                setUserLike(false)
-            } else {
-                setLikes(tweet.likes+1)
-                setUserLike(true)
-            }
-            
-            
-        }
+        apiTweetAction(tweet.id, action.type, handleActionBackendEvent)
+       
     }
     const display = action.type === 'like' ? `${likes} ${actionDisplay}` : actionDisplay
     return <button className={className} onClick={handleClick}>{display}</button>
