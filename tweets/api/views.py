33e26c751c3eee463edbae2fd7ca199bd4c23582
mohammadali <mohammadali.kalbasi6911@gmail.com>
2024-01-03 20:41:21 +0330

    95. More Efficient List Views with Pagination

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -6,6 +6,7 @@ from django.utils.http import is_safe_url
 
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from ..forms import TweetForm 
@@ -86,13 +87,19 @@ def tweet_action_view(request, *args, **kwargs):
     return Response({}, status=200)
 
 
+def get_paginated_queryset_response(qs, request, serializer):
+    paginator = PageNumberPagination()
+    paginator. page_size = 20
+    paginated_qs = paginator.paginate_queryset(qs, request)
+    serializer = TweetSerializer(paginated_qs, many=True)
+    return paginator.get_paginated_response(serializer.data)#Response(serializer.data, status=200)
+
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def tweet_feed_view(request, *args, **kwargs):
     user = request.user
     qs = Tweet.objects.feed(user)
-    serializer = TweetSerializer(qs, many=True)
-    return Response(serializer.data)
+    return get_paginated_queryset_response(qs, request, args)
 
 
 @api_view(['GET'])
